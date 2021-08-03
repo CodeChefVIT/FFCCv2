ae0a3b5d12eeb7f6620d46dc0e00546d68efd314
Sidharth Mishra <sidharth1604@gmail.com>
2021-08-03 23:13:22 +0530

    Changes Added

--- FCCv2_MainPage/course.js
+++ FCCv2_MainPage/course.js
@@ -1,111 +1,44 @@
-/*Fetching Courses*/
+let result = {};
+let allCourses = {};
 
-// let course_dropdown = document.getElementById("one");
-
-// async function getCourses(){
-//   var response = await fetch('https://ffcc-app.herokuapp.com/get/courses');
-//   var json_data = await response.json();
-
-//   console.log(json_data);
-//   course_dropdown.innerHTML="";
-
-//   Object.keys(json_data).forEach((item,index)=>{
-//     var opt = document.createElement("option");
-//     opt.text = item.title;
-//     opt.value = item.code;
-
-//     course_dropdown.appendChild(opt);
-//   });
-// }
-// getCourses();
-
-const point = 'https://ffcc-app.herokuapp.com/get/courses';
-
-let courses1 =[];
-
-fetch(point)
-    .then(response => response.json())
-    .then(data => courses1=data.courses)
-
-function findMatches(wordToMatch , courses1){
-    return courses1.filter(function place(a){
-        const regex = new RegExp(wordToMatch,'gi')
-        return a.code.match(regex) || a.title.match(regex)
-    });
+function filterCourses() {
+  var filter = document.getElementById("inputFilter").value;
+  result.courses = allCourses.courses.filter(x => x.title.toUpperCase().includes(filter.toUpperCase()))
+  let select = document.getElementById("one");
+  
+  while (select.firstChild) {
+        select.removeChild(select.firstChild);
+  }
+  
+  for (let i = 0; i < result.courses.length; i++){
+        let option = document.createElement("option");
+        option.value = result.courses[i]._id;
+        option.text = result.courses[i].title;
+        select.appendChild(option);
+    }
 }
 
-function displayMatches(){
-    const matchArray = findMatches(this.value, courses1)
-    const html = matchArray.map(place => {
-        const regex = new RegExp(this.value, 'gi');
-        const code = place.code.replace(regex, `<span class="hl">${this.value}</span>`);
-        const title = place.title.replace(regex, `<span class="hl">${this.value}</span>`);
-        return `
-        <div class="option">
-              <input type="radio" class="radio" name="category" />
-              <label for="course-1"> ${code} , ${title} , ${place.credits} , ${place.type}</label>
-        </div>
-        `
-    }).join("");
-    opt.innerHTML = html;
+async function getCourses() {
+    let url = 'users.json';
+    try {
+        let res = await fetch("https://ffcc-app.herokuapp.com/get/courses");
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
 }
-const search = document.querySelector(".search-box");
-const opt = document.querySelector(".options-container");
-search.addEventListener("change",displayMatches);
-search.addEventListener("keyup",displayMatches);
 
-/*Active toggle */
+async function renderCourses() {
+    allCourses = await getCourses();
+    result = Object.assign({}, allCourses);
+    let select = document.getElementById("one");
+    for (let i = 0; i < result.courses.length; i++){
+        let option = document.createElement("option");
+        option.value = result.courses[i]._id;
+        option.text = result.courses[i].title;
+        select.appendChild(option);
+    }
+}
 
-const selectedAll = document.querySelectorAll(".selected");
-selectedAll.forEach((selected) => {
-    const optionsContainer = selected.previousElementSibling;
-    const searchBox = selected.nextElementSibling;
-  
-    const optionsList = optionsContainer.querySelectorAll(".option");
-  
-    selected.addEventListener("click", () => {
-      if (optionsContainer.classList.contains("active")) {
-        optionsContainer.classList.remove("active");
-      } else {
-        let currentActive = document.querySelector(".options-container.active");
-  
-        if (currentActive) {
-          currentActive.classList.remove("active");
-        }
-  
-        optionsContainer.classList.add("active");
-      }
-  
-      searchBox.value = "";
-    //   filterList("");
-  
-      if (optionsContainer.classList.contains("active")) {
-        searchBox.focus();
-      }
-    });
-  
-    optionsList.forEach((o) => {
-      o.addEventListener("click", () => {
-        selected.innerHTML = o.querySelector("label").innerHTML;
-        optionsContainer.classList.remove("active");
-      });
-    });
-
-  
-    // searchBox.addEventListener("keyup", function (e) {
-    //   filterList(e.target.value);
-    // });
-  
-    // const filterList = (searchTerm) => {
-    //   searchTerm = searchTerm.toLowerCase();
-    //   optionsList.forEach((option) => {
-    //     let label = option.firstElementChild.nextElementSibling.innerText.toLowerCase();
-    //     if (label.indexOf(searchTerm) != -1) {
-    //       option.style.display = "block";
-    //     } else {
-    //       option.style.display = "none";
-    //     }
-    //   });
-    // };
-});
+renderCourses()
 
---
+++ FCCv2_MainPage/course2.js
@@ -0,0 +1,43 @@
+let result2 = {};
+let allCourses2 = {};
+
+function filterCourses() {
+  var filter = document.getElementById("inputFilter2").value;
+  result2.courses = allCourses2.courses.filter(x => x.title.toUpperCase().includes(filter.toUpperCase()))
+  let select = document.getElementById("two");
+  
+  while (select.firstChild) {
+        select.removeChild(select.firstChild);
+  }
+  
+  for (let i = 0; i < result2.courses.length; i++){
+        let option = document.createElement("option");
+        option.value = result2.courses[i]._id;
+        option.text = result2.courses[i].title;
+        select.appendChild(option);
+    }
+}
+
+async function getCourses() {
+    let url = 'users.json';
+    try {
+        let res = await fetch("https://ffcc-app.herokuapp.com/get/courses");
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function renderCourses() {
+    allCourses2 = await getCourses();
+    result2 = Object.assign({}, allCourses2);
+    let select = document.getElementById("two");
+    for (let i = 0; i < result2.courses.length; i++){
+        let option = document.createElement("option");
+        option.value = result2.courses[i]._id;
+        option.text = result2.courses[i].title;
+        select.appendChild(option);
+    }
+}
+
+renderCourses()
